0f4070501af9712322d7ebf2349ff1eecc0d4559
Unknown <xeonpowder@gmail.com>
2016-11-08 17:32:27 -0600

    minor edit

--- app/src/main.js
+++ app/src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Electron from 'vue-electron'
 import Resource from 'vue-resource'
 import Router from 'vue-router'
-
 import App from './App'
 import routes from './routes'
 

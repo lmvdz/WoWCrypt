ce66e84f9258387d2a4089ab8628bfa9784223d5
Unknown <xeonpowder@gmail.com>
2016-11-08 17:27:09 -0600

    added debug option to `npm run dev`

--- tasks/runner.js
+++ tasks/runner.js
@@ -58,4 +58,4 @@ function exit (code) {
 }
 
 console.log(`${YELLOW}Starting webpack-dev-server...\n${END}`)
-run(`webpack-dev-server --inline --hot --colors --port ${config.port} --content-base app/dist`, YELLOW, 'webpack')
+run(`webpack-dev-server --display-error-details --inline --hot --colors --port ${config.port} --content-base app/dist`, YELLOW, 'webpack')
